f0ca899a33c0c0bc478fd7b6e3d0233a50708c2c
Peter-Frank Spierenburg <peter@hal-con.com>
2025-02-01 22:26:35 -0400

    Moved main game server into main_game

---
+++ core/config.py
@@ -0,0 +1,12 @@
+import pygame
+
+BORDER_COLOUR = "green"
+BACKGROUND_COLOUR = "black"
+TEXT_BACKGROUND_COLOUR = "purple"
+
+BELL_SOUND = pygame.mixer.Sound("sounds/bell.mp3")
+BUZZER_SOUND = pygame.mixer.Sound("sounds/buzzer.mp3")
+DUPLICATE_SOUND = pygame.mixer.Sound("sounds/duplicate.mp3")
+REVEAL_SOUND = pygame.mixer.Sound("sounds/reveal.mp3")
+POINTS_SOUND = pygame.mixer.Sound("sounds/points.mp3")
+NO_POINTS_SOUND = pygame.mixer.Sound("sounds/no_points.mp3")
