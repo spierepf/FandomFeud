c611c92b3891ab636c7555ce74ffe2234cc99f18
Peter-Frank Spierenburg <peter@hal-con.com>
2025-02-02 13:15:21 -0400

    Copied fast money server into fast_money

--- fast_money/client.py
+++ fast_money/client.py
@@ -1,28 +1,100 @@
 #!/usr/bin/python3
+import json
 import pathlib
 import tkinter as tk
+from logging import exception
+from tkinter import ttk, filedialog, messagebox
+
 import pygubu
 from clientui import ClientUI
+import logging
+
+from os.path import dirname, abspath, join
+import sys
+sys.path.append(abspath(join(dirname(__file__), '..')))
+from core.rpc import RPCClient
+from side import Side
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 
 class Client(ClientUI):
     def __init__(self, master=None):
         super().__init__(master)
+        self.model = None
+        self.event_var = self.builder.get_variable("event_var")
+        self.disable_all(self.builder.get_object("round_frame"))
+
+    def _navigate_widget_tree(self, widget, cond=None, op=None):
+        if callable(getattr(widget, 'winfo_children', None)):
+            for child in widget.winfo_children():
+                self._navigate_widget_tree(child, cond, op)
+        if cond is None or cond(widget):
+            if op is not None:
+                op(widget)
+
+    def disable_all(self, widget):
+        self._navigate_widget_tree(widget,
+                                   lambda w: type(w) in [ttk.Button, ttk.Radiobutton, ttk.Entry],
+                                   lambda w: w.config(state=tk.DISABLED))
+
+    def enable_all(self, widget):
+        self._navigate_widget_tree(widget,
+                                   lambda w: type(w) in [ttk.Button, ttk.Radiobutton, ttk.Entry],
+                                   lambda w: w.config(state=tk.NORMAL))
 
     def connect_to_server(self):
-        pass
+        host = self.builder.get_variable("host_var").get()
+        port = self.builder.get_variable("port_var").get()
+        logger.info(f"Connecting to {host} on port {port}")
+        try:
+            self.model = RPCClient(host, port)
+            self.model.connect()
+            self.enable_all(self.builder.get_object("round_frame"))
+        except Exception as e:
+            logger.exception(f"While connecting to {host} on port {port}")
+            messagebox.showerror('Connection Error', str(e))
+        self.disable_all(self.builder.get_object("connect_frame"))
 
     def set_timer_duration(self):
-        pass
+        self.model.set_timer_duration(self.builder.get_variable('duration_entry_var').get())
 
     def start_timer(self):
-        pass
+        self.model.set_timer_start(True)
 
     def play_duplicate_sound(self):
-        pass
+        self.model.set_play_duplicate_sound(True)
 
     def commit(self):
-        pass
+        event = self.event_var.get()
+        logger.info(f"Committing {event}")
+        next_event = 'reveal_score_0'
+
+        match event:
+            case event if event.startswith('reveal_answer_'):
+                i = int(event[-1])
+                answer = self.builder.get_variable(f'answer_{i}_entry_var').get()
+                if answer is not None:
+                    self.model.set_answer(i, answer)
+                    self.model.set_play_reveal_sound(True)
+                    next_event = f'reveal_score_{i}'
+
+
+            case event if event.startswith('reveal_score_'):
+                i = int(event[-1])
+                next_event = f'reveal_answer_{(i+1)%10}'
+                score = self.builder.get_variable(f'score_{i}_entry_var').get()
+                if score is not None:
+                    self.model.set_score(i, score)
+                    if score > 0:
+                        self.model.set_play_points_sound(True)
+                    else:
+                        self.model.set_play_no_points_sound(True)
+
+        self.builder.get_variable('event_var').set(next_event)
+
+
 
 
 if __name__ == "__main__":
---
+++ fast_money/fast_money_model.py
@@ -0,0 +1,65 @@
+class FastMoneyModel:
+    def __init__(self):
+        self.timer_duration = 20
+        self.timer_end_time = None
+        self.timer_start = False
+        self.play_duplicate_sound = False
+        self.play_reveal_sound = False
+        self.play_points_sound = False
+        self.play_no_points_sound = False
+        self.answer = [None] * 10
+        self.score = [None] * 10
+
+    def get_timer_duration(self):
+        return self.timer_duration
+
+    def set_timer_duration(self, timer_time):
+        self.timer_duration = timer_time
+
+    def get_timer_end_time(self):
+        return self.timer_end_time
+
+    def set_timer_end_time(self, timer_end_time):
+        self.timer_end_time = timer_end_time
+
+    def get_timer_start(self):
+        return self.timer_start
+
+    def set_timer_start(self, timer_start):
+        self.timer_start = timer_start
+
+    def get_play_duplicate_sound(self):
+        return self.play_duplicate_sound
+
+    def set_play_duplicate_sound(self, play_duplicate_sound):
+        self.play_duplicate_sound = play_duplicate_sound
+
+    def get_play_reveal_sound(self):
+        return self.play_reveal_sound
+
+    def set_play_reveal_sound(self, play_reveal_sound):
+        self.play_reveal_sound = play_reveal_sound
+
+    def get_play_points_sound(self):
+        return self.play_points_sound
+
+    def set_play_points_sound(self, play_points_sound):
+        self.play_points_sound = play_points_sound
+
+    def get_play_no_points_sound(self):
+        return self.play_no_points_sound
+
+    def set_play_no_points_sound(self, play_no_points_sound):
+        self.play_no_points_sound = play_no_points_sound
+
+    def set_answer(self, index, answer):
+        self.answer[index] = answer
+
+    def get_answer(self, index):
+        return self.answer[index]
+
+    def set_score(self, index, score):
+        self.score[index] = score
+
+    def get_score(self, index):
+        return self.score[index]
