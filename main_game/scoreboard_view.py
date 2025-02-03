b9429cd5ca97f5c30dc5cc2049460ee1a8492138
Peter-Frank Spierenburg <peter@hal-con.com>
2025-02-03 09:28:22 -0400

    Removed RectBuilder from main_game

--- main_game/scoreboard_view.py
+++ main_game/scoreboard_view.py
@@ -5,7 +5,6 @@ import sys
 sys.path.append(abspath(join(dirname(__file__), '..')))
 from core.pygame_view import PygameView
 from core.config import BORDER_COLOUR, BACKGROUND_COLOUR, TEXT_BACKGROUND_COLOUR, BELL_SOUND, BUZZER_SOUND
-from core.rect_builder import RectBuilder
 from core.side import Side
 
 class LocalRect(pygame.Rect):
@@ -61,26 +60,22 @@ class ScoreboardView(PygameView):
 
     def draw_borders(self):
         # Main ellipse
-        pygame.draw.ellipse(self.surface, BORDER_COLOUR, RectBuilder(self.surface).size(85, 61).done(), int(self.UNIT))
-        # pygame.draw.ellipse(self.surface, BACKGROUND_COLOUR, RectBuilder(self.surface).size(84, 60).done())
+        pygame.draw.ellipse(self.surface, BORDER_COLOUR, LocalRect(self.surface.get_rect()).resize(85*self.UNIT, 61*self.UNIT), int(self.UNIT))
 
         # Left score
-        pygame.draw.rect(self.surface, BORDER_COLOUR, RectBuilder(self.surface).size(15, 10).translate(-41, 0).done(),
-                         0, int(self.UNIT))
-        pygame.draw.rect(self.surface, BACKGROUND_COLOUR,
-                         RectBuilder(self.surface).size(14, 9).translate(-41, 0).done(), 0, int(self.UNIT))
+        rect = LocalRect(self.surface.get_rect()).resize(15*self.UNIT, 10*self.UNIT).move(-41*self.UNIT, 0)
+        pygame.draw.rect(self.surface, BORDER_COLOUR, rect,0, int(self.UNIT))
+        pygame.draw.rect(self.surface, BACKGROUND_COLOUR, rect.inflate(-self.UNIT, -self.UNIT), 0, int(self.UNIT))
 
         # Right score
-        pygame.draw.rect(self.surface, BORDER_COLOUR, RectBuilder(self.surface).size(15, 10).translate(+41, 0).done(),
-                         0, int(self.UNIT))
-        pygame.draw.rect(self.surface, BACKGROUND_COLOUR,
-                         RectBuilder(self.surface).size(14, 9).translate(+41, 0).done(), 0, int(self.UNIT))
+        rect = LocalRect(self.surface.get_rect()).resize(15*self.UNIT, 10*self.UNIT).move(+41*self.UNIT, 0)
+        pygame.draw.rect(self.surface, BORDER_COLOUR, rect,0, int(self.UNIT))
+        pygame.draw.rect(self.surface, BACKGROUND_COLOUR, rect.inflate(-self.UNIT, -self.UNIT), 0, int(self.UNIT))
 
         # Inner border
-        pygame.draw.rect(self.surface, BORDER_COLOUR, RectBuilder(self.surface).size(self.inner_border_width, self.inner_border_height).done(), 0,
-                         int(self.UNIT))
-        pygame.draw.rect(self.surface, BACKGROUND_COLOUR, RectBuilder(self.surface).size(self.inner_border_width - 1, self.inner_border_height - 1).done(), 0,
-                         int(self.UNIT))
+        rect = LocalRect(self.surface.get_rect()).resize(self.inner_border_width * self.UNIT, self.inner_border_height * self.UNIT)
+        pygame.draw.rect(self.surface, BORDER_COLOUR, rect, 0, int(self.UNIT))
+        pygame.draw.rect(self.surface, BACKGROUND_COLOUR, rect.inflate(-self.UNIT, -self.UNIT), 0, int(self.UNIT))
 
     def draw_score(self, score, x, y):
         rect = LocalRect(self.surface.get_rect().move(x * self.UNIT, y * self.UNIT)).resize(self.UNIT * 13, self.UNIT * 8)
@@ -114,17 +109,13 @@ class ScoreboardView(PygameView):
                         self.draw_hidden_answer(cell, i)
                 i += 1
 
-    def draw_x(self, count):
-        for i in range(count):
-            x = 32 * (i - 0.5 * count + 0.5)
-            pygame.draw.rect(self.surface, "black", RectBuilder(self.surface).size(34, 34).translate(x, 0).done(), 0,
-                             int(self.UNIT * 3))
-            pygame.draw.rect(self.surface, "red", RectBuilder(self.surface).size(30, 30).translate(x, 0).done(),
-                             int(4 * self.UNIT),
-                             int(self.UNIT * 2))
+    def draw_strike(self, count):
+        for rect in LocalRect(self.surface.get_rect()).resize(self.UNIT * 32 * count, self.UNIT * 32).columns(count):
+            pygame.draw.rect(self.surface, "black", rect.resize(self.UNIT * 34, self.UNIT * 34), 0, int(self.UNIT * 3))
+            pygame.draw.rect(self.surface, "red", rect.resize(self.UNIT * 30, self.UNIT * 30), int(4 * self.UNIT), int(self.UNIT * 2))
             font = pygame.font.Font("fonts/NimbusSans-Regular.otf", int(self.UNIT * 47))
             shadow = font.render("\u00D7", True, "red")
-            self.surface.blit(shadow, RectBuilder(self.surface, shadow.get_rect()).translate(x, 0).done())
+            self.surface.blit(shadow, rect.resize(shadow.get_width(), shadow.get_height()))
 
     def draw_scores(self):
         self.draw_score(self.model.get_score(Side.LEFT), -41, 0)  # left score
@@ -145,7 +136,7 @@ class ScoreboardView(PygameView):
                 self.model.set_strike_start_time(pygame.time.get_ticks())
                 BUZZER_SOUND.play()
             elif pygame.time.get_ticks() - self.model.get_strike_start_time() < int(BUZZER_SOUND.get_length() * 1000):
-                self.draw_x(self.model.get_strike())
+                self.draw_strike(self.model.get_strike())
             else:
                 self.model.clear_strike()
 
