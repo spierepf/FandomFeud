68daa4a43241093f6156cfd7cc7fe664a6566e23
Peter-Frank Spierenburg <peter-frank.spierenburg@hal-con.com>
2025-05-17 09:04:40 -0300

    Updated client UIs

--- fast_money/clientui.py
+++ fast_money/clientui.py
@@ -9,18 +9,8 @@ RESOURCE_PATHS = [PROJECT_PATH]
 
 
 class ClientUI:
-    def __init__(
-        self,
-        master=None,
-        translator=None,
-        on_first_object_cb=None,
-        data_pool=None
-    ):
-        self.builder = pygubu.Builder(
-            translator=translator,
-            on_first_object=on_first_object_cb,
-            data_pool=data_pool
-        )
+    def __init__(self, master=None):
+        self.builder = pygubu.Builder()
         self.builder.add_resource_paths(RESOURCE_PATHS)
         self.builder.add_from_file(PROJECT_UI)
         # Main widget
